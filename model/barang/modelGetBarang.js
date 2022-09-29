63c849ba527669a9e5d35528ce7344496840583d
taufik17 <taufikagungsantoso17@gmail.com>
2022-09-29 07:28:42 +0700

    [fix] query barang

--- model/barang/modelGetBarang.js
+++ model/barang/modelGetBarang.js
@@ -3,7 +3,9 @@ const db = require("../../db");
 const allBarang = () =>
   new Promise((resolve, reject) => {
     db.query(
-      "SELECT * FROM barang ORDER BY barang.id_barang ASC",
+      `SELECT * FROM barang
+      INNER JOIN jenis ON jenis.id_jenis = barang.jenis_barang
+      ORDER BY barang.id_barang ASC`,
       (error, results) => {
         if (error) {
           reject(error);
