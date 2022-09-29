a70c84c9bcd4a3d5f61327ffc1ed61e279b898fb
taufik17 <taufikagungsantoso17@gmail.com>
2022-09-29 10:14:52 +0700

    [fix] jenis by id

--- controllers/jenis/controllerGetJenis.js
+++ controllers/jenis/controllerGetJenis.js
@@ -9,6 +9,17 @@ const getAllJenis = async (req, res) => {
   }
 };
 
+const getJenisById = async (req, res) => {
+  try {
+    const { id_jenis } = req.body;
+    const getData = await model.getByID(id_jenis);
+    res.send({ data: getData.rows, jumlahData: getData.rowCount });
+  } catch (error) {
+    res.status(400).send("ada yang error");
+  }
+};
+
 module.exports = {
   getAllJenis,
+  getJenisById,
 };
--- routes/jenis/get_jenis.js
+++ routes/jenis/get_jenis.js
@@ -2,5 +2,6 @@ const Router = require("express").Router();
 const controller = require("../../controllers/jenis/controllerGetJenis");
 
 Router.get("/jenis", controller.getAllJenis);
+Router.post("/jenis/id", controller.getJenisById);
 
 module.exports = Router;
